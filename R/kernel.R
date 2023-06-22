356d0bb63e965d62edc760a8abf4d5c6d8f66968
AnHosu <andellegaard@gmail.com>
2023-06-22 16:59:09 +0000

    started experimenting with a kernel class

---
+++ R/kernel.R
@@ -0,0 +1,16 @@
+k <- structure(
+  list(),
+  class = "seoro_kernel"
+)
+
+new_kernel <- function(parameters, fixed_parameters) {
+  
+}
+
+validate_kernel <- function() {
+  
+}
+
+kernel <- function() {
+  
+}
