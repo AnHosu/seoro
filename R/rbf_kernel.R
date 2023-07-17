4ae92d449d0e45410566443f947760b549bdf809
AnHosu <andellegaard@gmail.com>
2023-07-17 20:07:55 +0000

    updated rbf kernel

--- R/rbf_kernel.R
+++ R/rbf_kernel.R
@@ -4,15 +4,30 @@
 #' The RBF kernel is defined as:
 #' \deqn{ k(x1, x2) = e^( -0.5(||x1 - x2|| / l)^2) }
 #'
-#' @param x1 First numeric vector of length d
-#' @param x2 Second numeric vector of length d
 #' @param l Length scale. Scalar or numeric vector of length d.
 #'
 #' @return scalar
 #' @export
 #'
 #' @examples
-#' rbf_kernel(rnorm(4), rnorm(4, 1, 2), l = 3)
-rbf_kernel <- function(x1, x2, l = 1.0) {
-  exp(- 0.5 * sum(((x1 - x2) / l)^2))
+#' k <- rbf_kernel(l = 3)
+#' k(rnorm(4), rnorm(4, 1, 2))
+rbf_kernel <- function(l = 1) {
+  .exp <- rlang::quo(
+    (- 2*(x1 %*% t(x2))) %>%
+      add(rowSums(x1**2, dims = 1)) %>%
+      sweep(2, rowSums(x2**2, dims = 1), `+`) %>%
+      multiply_by(-0.5 / l**2) %>%
+      exp()
+  )
+  structure(
+    function(x1, x2) {
+      if (is.null(dim(x1))) dim(x1) <- c(1, length(x1))
+      if (is.null(dim(x2))) dim(x2) <- c(1, length(x2))
+      rlang::eval_tidy(.exp, data = list(x1 = x1, x2 = x2))
+    },
+    .exp = exp,
+    .quo = rlang::set_names(rlang::quos(!!.exp), "rbf"),
+    class = "seoro_kernel"
+  )
 }
