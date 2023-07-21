35e291b1c1adbbe781d547cc2957f78f66cae3cf
AnHosu <andellegaard@gmail.com>
2023-07-21 19:44:24 +0000

    added new_kernel function

---
+++ R/new_kernel.R
@@ -0,0 +1,18 @@
+new_kernel <- function(expr, name, ...) {
+  expr <- rlang::enexpr(expr)
+  f <- function(l = 1) {
+    .exp <- rlang::quo(!!expr)
+    structure(
+      function(x1, x2) {
+        if (is.null(dim(x1))) dim(x1) <- c(1, length(x1))
+        if (is.null(dim(x2))) dim(x2) <- c(1, length(x2))
+        rlang::eval_tidy(.exp, data = list(x1 = x1, x2 = x2))
+      },
+      .exp = .exp,
+      .quo = rlang::set_names(rlang::quos(!!.exp), name),
+      class = "seoro_kernel"
+    )
+  }
+  formals(f) <- rlang::pairlist2(...)
+  return(f)
+}
