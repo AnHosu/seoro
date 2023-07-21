c14caf7016ec4ab7ca7edeff1434b5c299cf759f
AnHosu <andellegaard@gmail.com>
2023-07-21 19:44:49 +0000

    applied kernel constructor to rbf kernel

--- R/rbf_kernel.R
+++ R/rbf_kernel.R
@@ -13,22 +13,14 @@
 #' @examples
 #' k <- rbf_kernel(l = 3)
 #' k(rnorm(4), rnorm(4, 1, 2))
-rbf_kernel <- function(l = 1) {
-  .exp <- rlang::quo(
+rbf_kernel <- new_kernel(
+  expr = {
     (- 2*(x1 %*% t(x2))) %>%
       `+`(rowSums(x1**2, dims = 1)) %>%
       sweep(2, rowSums(x2**2, dims = 1), `+`) %>%
       `*`(-0.5 / l**2) %>%
       exp()
-  )
-  structure(
-    function(x1, x2) {
-      if (is.null(dim(x1))) dim(x1) <- c(1, length(x1))
-      if (is.null(dim(x2))) dim(x2) <- c(1, length(x2))
-      rlang::eval_tidy(.exp, data = list(x1 = x1, x2 = x2))
-    },
-    .exp = .exp,
-    .quo = rlang::set_names(rlang::quos(!!.exp), "rbf"),
-    class = "seoro_kernel"
-  )
-}
+  },
+  name = "rbf",
+  l = 1
+)
