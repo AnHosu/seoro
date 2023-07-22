796ac5690511589b81b3cebf64bc49c2957b30f9
AnHosu <andellegaard@gmail.com>
2023-07-22 19:06:04 +0000

    used constructor for constant kernel

--- R/constant_kernel.R
+++ R/constant_kernel.R
@@ -12,16 +12,10 @@
 #' @examples
 #' k <- constant_kernel(sigma = 2)
 #' k(rnorm(4), rnorm(4, 1, 2))
-constant_kernel <- function(sigma = 1) {
-  .exp <- rlang::quo(matrix(sigma^2, dim(x1)[[1]], dim(x2)[[1]]))
-  structure(
-    function(x1, x2) {
-      if (is.null(dim(x1))) dim(x1) <- c(1, length(x1))
-      if (is.null(dim(x2))) dim(x2) <- c(1, length(x2))
-      rlang::eval_tidy(.exp, data = list(x1 = x1, x2 = x2))
-    },
-    .exp = .exp,
-    .quo = rlang::set_names(rlang::quos(!!.exp), "constant"),
-    class = "seoro_kernel"
-  )
-}
+constant_kernel <- new_kernel(
+  expr = {
+    matrix(sigma^2, dim(x1)[[1]], dim(x2)[[1]])
+  },
+  name = "constant",
+  sigma = 1
+)
